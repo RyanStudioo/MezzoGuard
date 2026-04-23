f6291619dc106b9b4a5ef91dafc225c9e049beec
RyanStudioo2 <150920961+RyanStudioo@users.noreply.github.com>
2026-04-23 11:31:02 +0800

    content guard categories

--- src/mezzoguard/modules/content_guard/moderation_categories.py
+++ src/mezzoguard/modules/content_guard/moderation_categories.py
@@ -7,3 +7,8 @@ class ContentGuardModerationCategories(Enum):
     TOXIC = "Toxic"
     DIVISIVE = "Divisive"
     SELF_HARM = "Self-Harm"
+
+class ContentGuardCategory:
+    def __init__(self, category: ContentGuardModerationCategories, confidence: float):
+        self.category = category
+        self.confidence = confidence
