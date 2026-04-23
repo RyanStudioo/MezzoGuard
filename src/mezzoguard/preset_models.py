c28cc51a20a16cf027762553c5e3103df5db958d
RyanStudioo2 <150920961+RyanStudioo@users.noreply.github.com>
2026-04-23 09:33:33 +0800

    model name updates

--- src/mezzoguard/preset_models.py
+++ src/mezzoguard/preset_models.py
@@ -1,12 +1,10 @@
 class PromptGuardModels:
 
     # Mezzo Prompt Guard v2 Models
-    MEZZO_V2_LARGE = "RyanStudio/Mezzo-Prompt-Guard-v2-Large"
-    MEZZO_V2_BASE = "RyanStudio/Mezzo-Prompt-Guard-v2-Base"
-    MEZZO_V2_SMALL = "RyanStudio/Mezzo-Prompt-Guard-v2-Small"
+    MEZZO_PROMPT_GUARD_V2_LARGE = "RyanStudio/Mezzo-Prompt-Guard-v2-Large"
+    MEZZO_PROMPT_GUARD_V2_BASE = "RyanStudio/Mezzo-Prompt-Guard-v2-Base"
+    MEZZO_PROMPT_GUARD_V2_SMALL = "RyanStudio/Mezzo-Prompt-Guard-v2-Small"
 
 class ContentGuardModels:
 
-    MEZZO_CONTENT_GUARD_LARGE = "RyanStudio/Mezzo-Content-Guard-Large"
-    MEZZO_CONTENT_GUARD_BASE = "RyanStudio/Mezzo-Content-Guard-Base"
-    MEZZO_CONTENT_GUARD_SMALL = "RyanStudio/Mezzo-Content-Guard-Small"
+    MEZZO_CONTENT_GUARD_PREVIEW = "RyanStudio/Mezzo-Content-Guard-Preview"
