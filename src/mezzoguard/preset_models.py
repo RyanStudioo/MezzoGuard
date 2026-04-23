0745a36ebe0f4b5d33e13d8bc6d8cd13f8efd922
RyanStudioo2 <150920961+RyanStudioo@users.noreply.github.com>
2026-04-23 09:41:15 +0800

    repo name change

--- src/mezzoguard/preset_models.py
+++ src/mezzoguard/preset_models.py
@@ -1,10 +1,10 @@
-class PromptGuardModels:
+class PROMPTGUARD:
 
     # Mezzo Prompt Guard v2 Models
     MEZZO_PROMPT_GUARD_V2_LARGE = "RyanStudio/Mezzo-Prompt-Guard-v2-Large"
     MEZZO_PROMPT_GUARD_V2_BASE = "RyanStudio/Mezzo-Prompt-Guard-v2-Base"
     MEZZO_PROMPT_GUARD_V2_SMALL = "RyanStudio/Mezzo-Prompt-Guard-v2-Small"
 
-class ContentGuardModels:
+class CONTENTGUARD:
 
-    MEZZO_CONTENT_GUARD_PREVIEW = "RyanStudio/Mezzo-Content-Guard-Preview"
+    MEZZO_CONTENT_GUARD_LARGE_PREVIEW = "RyanStudio/Mezzo-Content-Guard-Large-Preview"
